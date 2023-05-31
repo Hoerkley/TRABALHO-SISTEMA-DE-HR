d6ddac2624ee0c0217971d8278aec61a83c00e70
Hoerkley <hoerkley.santos@aluno.senai.br>
2023-05-30 21:29:31 -0300

    Adiconado interface no formulário de horas extras

---
+++ iRh/iRh.Windows/Core/HoraExtra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRh.Windows.Core
+{
+    internal class HoraExtra
+    {
+        public static double Calcula(double salraio)
+        {
+            
+            return 0;
+        }
+    }
+}
--- iRh/iRh.Windows/Simuladores/frmBeneficioHoraExtra.Designer.cs
+++ iRh/iRh.Windows/Simuladores/frmBeneficioHoraExtra.Designer.cs
@@ -28,12 +28,209 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.rbDomingosFeriadosSim = new System.Windows.Forms.CheckBox();
+            this.lblSalrioFuncionarioHoraExtra = new System.Windows.Forms.Label();
+            this.txtSalarioHoraExtra = new System.Windows.Forms.TextBox();
+            this.lblDomingosFeriados = new System.Windows.Forms.Label();
+            this.checkBox1rbDomingosFeriadosNao = new System.Windows.Forms.CheckBox();
+            this.lblHoraDoingosFeriados = new System.Windows.Forms.Label();
+            this.txtHoraDomingosFeriados = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtHoraDiasUteis = new System.Windows.Forms.TextBox();
+            this.lblHoraDiasUteis = new System.Windows.Forms.Label();
+            this.lblDiasUteis = new System.Windows.Forms.Label();
+            this.rbDiasUteisNao = new System.Windows.Forms.CheckBox();
+            this.rbDiasUteisSim = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtHoraDomingosFeriados);
+            this.panel1.Controls.Add(this.lblHoraDoingosFeriados);
+            this.panel1.Location = new System.Drawing.Point(4, 184);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(401, 79);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(12, 287);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(184, 66);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // rbDomingosFeriadosSim
+            // 
+            this.rbDomingosFeriadosSim.AutoSize = true;
+            this.rbDomingosFeriadosSim.Location = new System.Drawing.Point(16, 132);
+            this.rbDomingosFeriadosSim.Name = "rbDomingosFeriadosSim";
+            this.rbDomingosFeriadosSim.Size = new System.Drawing.Size(43, 17);
+            this.rbDomingosFeriadosSim.TabIndex = 2;
+            this.rbDomingosFeriadosSim.Text = "Sim";
+            this.rbDomingosFeriadosSim.UseVisualStyleBackColor = true;
+            // 
+            // lblSalrioFuncionarioHoraExtra
+            // 
+            this.lblSalrioFuncionarioHoraExtra.AutoSize = true;
+            this.lblSalrioFuncionarioHoraExtra.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalrioFuncionarioHoraExtra.Location = new System.Drawing.Point(12, 9);
+            this.lblSalrioFuncionarioHoraExtra.Name = "lblSalrioFuncionarioHoraExtra";
+            this.lblSalrioFuncionarioHoraExtra.Size = new System.Drawing.Size(257, 21);
+            this.lblSalrioFuncionarioHoraExtra.TabIndex = 3;
+            this.lblSalrioFuncionarioHoraExtra.Text = "Informe o salário do funcuinário";
+            // 
+            // txtSalarioHoraExtra
+            // 
+            this.txtSalarioHoraExtra.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalarioHoraExtra.Location = new System.Drawing.Point(16, 44);
+            this.txtSalarioHoraExtra.Name = "txtSalarioHoraExtra";
+            this.txtSalarioHoraExtra.Size = new System.Drawing.Size(100, 29);
+            this.txtSalarioHoraExtra.TabIndex = 4;
+            // 
+            // lblDomingosFeriados
+            // 
+            this.lblDomingosFeriados.AutoSize = true;
+            this.lblDomingosFeriados.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDomingosFeriados.Location = new System.Drawing.Point(12, 96);
+            this.lblDomingosFeriados.Name = "lblDomingosFeriados";
+            this.lblDomingosFeriados.Size = new System.Drawing.Size(393, 21);
+            this.lblDomingosFeriados.TabIndex = 5;
+            this.lblDomingosFeriados.Text = "O funcionário trabalhou em domingos e feriados? ";
+            // 
+            // checkBox1rbDomingosFeriadosNao
+            // 
+            this.checkBox1rbDomingosFeriadosNao.AutoSize = true;
+            this.checkBox1rbDomingosFeriadosNao.Location = new System.Drawing.Point(120, 132);
+            this.checkBox1rbDomingosFeriadosNao.Name = "checkBox1rbDomingosFeriadosNao";
+            this.checkBox1rbDomingosFeriadosNao.Size = new System.Drawing.Size(46, 17);
+            this.checkBox1rbDomingosFeriadosNao.TabIndex = 6;
+            this.checkBox1rbDomingosFeriadosNao.Text = "Não";
+            this.checkBox1rbDomingosFeriadosNao.UseVisualStyleBackColor = true;
+            // 
+            // lblHoraDoingosFeriados
+            // 
+            this.lblHoraDoingosFeriados.AutoSize = true;
+            this.lblHoraDoingosFeriados.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoraDoingosFeriados.Location = new System.Drawing.Point(8, 10);
+            this.lblHoraDoingosFeriados.Name = "lblHoraDoingosFeriados";
+            this.lblHoraDoingosFeriados.Size = new System.Drawing.Size(222, 21);
+            this.lblHoraDoingosFeriados.TabIndex = 7;
+            this.lblHoraDoingosFeriados.Text = "Informe as horas trabalhdas";
+            // 
+            // txtHoraDomingosFeriados
+            // 
+            this.txtHoraDomingosFeriados.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHoraDomingosFeriados.Location = new System.Drawing.Point(12, 43);
+            this.txtHoraDomingosFeriados.Name = "txtHoraDomingosFeriados";
+            this.txtHoraDomingosFeriados.Size = new System.Drawing.Size(100, 29);
+            this.txtHoraDomingosFeriados.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtHoraDiasUteis);
+            this.panel2.Controls.Add(this.lblHoraDiasUteis);
+            this.panel2.Location = new System.Drawing.Point(426, 184);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(401, 79);
+            this.panel2.TabIndex = 8;
+            // 
+            // txtHoraDiasUteis
+            // 
+            this.txtHoraDiasUteis.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHoraDiasUteis.Location = new System.Drawing.Point(12, 43);
+            this.txtHoraDiasUteis.Name = "txtHoraDiasUteis";
+            this.txtHoraDiasUteis.Size = new System.Drawing.Size(100, 29);
+            this.txtHoraDiasUteis.TabIndex = 7;
+            // 
+            // lblHoraDiasUteis
+            // 
+            this.lblHoraDiasUteis.AutoSize = true;
+            this.lblHoraDiasUteis.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoraDiasUteis.Location = new System.Drawing.Point(8, 10);
+            this.lblHoraDiasUteis.Name = "lblHoraDiasUteis";
+            this.lblHoraDiasUteis.Size = new System.Drawing.Size(222, 21);
+            this.lblHoraDiasUteis.TabIndex = 7;
+            this.lblHoraDiasUteis.Text = "Informe as horas trabalhdas";
+            // 
+            // lblDiasUteis
+            // 
+            this.lblDiasUteis.AutoSize = true;
+            this.lblDiasUteis.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiasUteis.Location = new System.Drawing.Point(422, 96);
+            this.lblDiasUteis.Name = "lblDiasUteis";
+            this.lblDiasUteis.Size = new System.Drawing.Size(298, 21);
+            this.lblDiasUteis.TabIndex = 9;
+            this.lblDiasUteis.Text = "O funcionário trabalhou em dias uteis";
+            // 
+            // rbDiasUteisNao
+            // 
+            this.rbDiasUteisNao.AutoSize = true;
+            this.rbDiasUteisNao.Location = new System.Drawing.Point(561, 132);
+            this.rbDiasUteisNao.Name = "rbDiasUteisNao";
+            this.rbDiasUteisNao.Size = new System.Drawing.Size(46, 17);
+            this.rbDiasUteisNao.TabIndex = 11;
+            this.rbDiasUteisNao.Text = "Não";
+            this.rbDiasUteisNao.UseVisualStyleBackColor = true;
+            // 
+            // rbDiasUteisSim
+            // 
+            this.rbDiasUteisSim.AutoSize = true;
+            this.rbDiasUteisSim.Location = new System.Drawing.Point(457, 132);
+            this.rbDiasUteisSim.Name = "rbDiasUteisSim";
+            this.rbDiasUteisSim.Size = new System.Drawing.Size(43, 17);
+            this.rbDiasUteisSim.TabIndex = 10;
+            this.rbDiasUteisSim.Text = "Sim";
+            this.rbDiasUteisSim.UseVisualStyleBackColor = true;
+            // 
+            // frmBeneficioHoraExtra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(952, 498);
+            this.Controls.Add(this.rbDiasUteisNao);
+            this.Controls.Add(this.rbDiasUteisSim);
+            this.Controls.Add(this.lblDiasUteis);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.checkBox1rbDomingosFeriadosNao);
+            this.Controls.Add(this.lblDomingosFeriados);
+            this.Controls.Add(this.txtSalarioHoraExtra);
+            this.Controls.Add(this.lblSalrioFuncionarioHoraExtra);
+            this.Controls.Add(this.rbDomingosFeriadosSim);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmBeneficioHoraExtra";
             this.Text = "frmBeneficioHoraExtra";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtHoraDomingosFeriados;
+        private System.Windows.Forms.Label lblHoraDoingosFeriados;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.CheckBox rbDomingosFeriadosSim;
+        private System.Windows.Forms.Label lblSalrioFuncionarioHoraExtra;
+        private System.Windows.Forms.TextBox txtSalarioHoraExtra;
+        private System.Windows.Forms.Label lblDomingosFeriados;
+        private System.Windows.Forms.CheckBox checkBox1rbDomingosFeriadosNao;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtHoraDiasUteis;
+        private System.Windows.Forms.Label lblHoraDiasUteis;
+        private System.Windows.Forms.Label lblDiasUteis;
+        private System.Windows.Forms.CheckBox rbDiasUteisNao;
+        private System.Windows.Forms.CheckBox rbDiasUteisSim;
     }
 }
